a4ef6c36ec639c700abbd12ae5b95f7a6490ff26
Guilherme Estephano Marques Alves <guiestephano@gmail.com>
2023-08-27 13:32:04 -0300

    Other entities and second migration

---
+++ SalesWebMvc/Models/SelesRecord.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using SalesWebMvc.Models.Enums;
+
+
+namespace SalesWebMvc.Models
+{
+    public class SelesRecord
+    {
+        public int Id { get; set; }
+        public DateTime Date { get; set; }
+        public double Amount { get; set; }
+        public SeleStatus Status { get; set; }
+        public Seller Seller { get; set; }
+
+        public SelesRecord() { }
+        public SelesRecord(int id, DateTime date, double amount, SeleStatus status, Seller seller)
+        {
+            Id = id;
+            Date = date;
+            Amount = amount;
+            Status = status;
+            Seller = seller;
+        }
+    }
+}
